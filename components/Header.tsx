68074915a6e79df72a0b4e719e1c051bb3df3bf2
Dominik <gradiskid@gmail.com>
2022-07-27 23:59:45 +0200

    adding logo and layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
+import { BeakerIcon, HomeIcon } from '@heroicons/react/solid'
+import { StarIcon } from '@heroicons/react/outline'
 
 function Header() {
   return (
       <div>
-          <div className=''>
-            <Image src="https://links.papareact.com/fqy" layout='fill'/>
+          <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
+            <Image 
+            objectFit='contain' 
+            src="https://links.papareact.com/fqy"
+            layout='fill'/>
+          </div>
+          <div>
+            <HomeIcon />
+            <p>Home</p>
           </div>
       </div>
   )
 }
-
+  
 export default Header
