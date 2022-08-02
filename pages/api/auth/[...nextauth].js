7612887954f5ba84086aa8a3059e0edcf9b83e98
Dominik <gradiskid@gmail.com>
2022-08-02 23:38:20 +0200

    added Reddit provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import RedditProvider from "next-auth/providers/reddit"
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
+    RedditProvider({
       clientId: process.env.REDDIT_CLIENT_ID,
       clientSecret: process.env.REDDIT_CLIENT_SECRET,
     }),
