7b614caeceda6895c05510ed6c682bc462b92ae8
Dominik <gradiskid@gmail.com>
2022-08-03 02:08:11 +0200

    added Session logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,25 @@
 import Image from "next/image";
 import React from "react";
-import { 
-    BeakerIcon, 
-    ChevronDownIcon,
-    HomeIcon,
-    SearchIcon } from "@heroicons/react/solid";
-import { 
-    BellIcon,
-    ChatIcon,
-    GlobeIcon,
-    MenuIcon,
-    PlusIcon,
-    SparklesIcon,
-    SpeakerphoneIcon,
-    VideoCameraIcon,
-  } from "@heroicons/react/outline";
-import { signIn } from "next-auth/react";
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  SearchIcon,
+} from "@heroicons/react/solid";
+import {
+  BellIcon,
+  ChatIcon,
+  GlobeIcon,
+  MenuIcon,
+  PlusIcon,
+  SparklesIcon,
+  SpeakerphoneIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -35,47 +37,76 @@ function Header() {
       </div>
 
       {/* Search box */}
-      <form className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 
-      bg-gray-100 px-3 py-1">
-          <SearchIcon className="h-6 w-6 text-gray-400"/>
-          <input 
+      <form
+        className="flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 
+      bg-gray-100 px-3 py-1"
+      >
+        <SearchIcon className="h-6 w-6 text-gray-400" />
+        <input
           className="flex-1 bg-transparent outline-none"
-           type="text" 
-           placeholder="Search Reddit" 
-           />
-          <button type="submit" hidden/>
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden />
       </form>
-      
-      <div className="flex text-gray-500 space-x-2 items-center mx-5 
-      hidden lg:inline-flex">
-        <SparklesIcon className="icon"/>
-        <GlobeIcon className="icon"/>
-        <VideoCameraIcon className="icon"/>
+
+      <div
+        className="flex text-gray-500 space-x-2 items-center mx-5 
+      hidden lg:inline-flex"
+      >
+        <SparklesIcon className="icon" />
+        <GlobeIcon className="icon" />
+        <VideoCameraIcon className="icon" />
         <hr className="h-10 border border-gray-100" />
-        <ChatIcon className="icon"/>
-        <BellIcon className="icon"/>
-        <PlusIcon className="icon"/>
-        <SpeakerphoneIcon className="icon"/>
-      
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <SpeakerphoneIcon className="icon" />
       </div>
       <div className="ml-5 flex items-center lg:hidden">
-      {/* implement burger-menu */}
-      <MenuIcon className="icon"/>
+        {/* implement burger-menu */}
+        <MenuIcon className="icon" />
       </div>
 
       {/* Sign in / Sign Out button */}
-      <div onClick={() => signIn()} className="hidden lg:flex items-center space-x-2 border-gray-100 p-2 cursor-pointer">
-        <div className="relative h-5 w-5 flex-shrink-0">
-        <Image 
-        objectFit="contain"
-        src="https://links.papareact.com/23l" 
-        layout="fill"
-        alt="" />
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt=""
+            />
+          </div>
 
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+          <p className="text-gray-400">1 Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
         </div>
-        
-        <p className="text-gray-400">Sign in</p>
-      </div>
+
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border-gray-100 p-2 cursor-pointer"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt=""
+            />
+          </div>
+
+          <p className="text-gray-400">Sign in</p>
+        </div>
+      )}
     </div>
   );
 }
