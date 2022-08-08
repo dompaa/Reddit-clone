4e6645f8adcf5b59d9637a68db2bf48696bb44b1
Dominik <gradiskid@gmail.com>
2022-08-08 23:58:01 +0200

    added photo and link attachments

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,11 +1,13 @@
 import { useSession } from 'next-auth/react';
 import React from 'react'
 import Avatar from './Avatar';
+import {LinkIcon, PhotographIcon } from '@heroicons/react/outline'
 
 function PostBox() {
     const { data: session} = useSession();
 
-  return <form>
+  return (
+  <form className='sticky top-16 z-50 bg-white border-rounded-md border-gray-300 p-2'>
     <div className='flex items-center space-x-3'>
         {/* Avatar */}
         <Avatar/>
@@ -14,9 +16,14 @@ function PostBox() {
         disabled={!session}
         className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
         type="text" 
-        placeholder={session ? 'Create a post by entering a title!' : 'Sign in to post'} />
+        placeholder={session ? 'Create a post by entering a title!' : 'Sign in to post'} 
+        />
+
+        <PhotographIcon className={`h-6 text-gray-300`} />
+        <LinkIcon className='h-6 text-gray-300'/>
     </div>
     </form>
+  )
 }
 
 export default PostBox
