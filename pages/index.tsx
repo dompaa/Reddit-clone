f27cd6e7c9bfd88554917ad1e9de7bcca28d4ba6
Dominik <gradiskid@gmail.com>
2022-08-14 23:59:05 +0200

    included Feed page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
@@ -14,6 +15,7 @@ const Home: NextPage = () => {
       
       <div className='flex'>
         {/* Feed */}
+        <Feed/>
       </div>
     </div>
   )
