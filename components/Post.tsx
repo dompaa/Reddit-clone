d73866c0332c938dcdfa6212f363917a1545c2f3
Dominik <gradiskid@gmail.com>
2022-08-19 01:04:08 +0200

    designed header/body/image/footer and timeAgo package

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React from "react";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  BookmarkIcon,
+  ChatAltIcon,
+  DotsHorizontalIcon,
+  GiftIcon,
+  ShareIcon,
+} from "@heroicons/react/outline";
+import Avatar from "./Avatar";
+import TimeAgo from 'react-timeago'
 
+type Props = {
+  post: Post;
+};
 
-function Post() {
+function Post({post}: Props) {
   return (
-    <div>Post</div>
-  )
+    <div className="flex cursor-pointer rounded-md border-gray-300 bg-white
+    shadow-sm hover:border-gray-600">
+      {/* Votes */}
+      <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md
+      bg-gray-50 p-4 text-gray-400">
+        <ArrowUpIcon className="voteButtons hover:text-red-400"/>
+        <p className="text-xs font-bold text-black">0</p>
+        <ArrowDownIcon className="voteButtons hover:text-blue-400"/>
+      </div>
+
+      <div className="p-3 pb-1">
+        {/* Header */}
+        {/* ERROR: NOT PASSING/SHOWING THE AVATAR, USERNAME,  */}
+        <div className="flex items-center space-x-2">
+          <Avatar seed={post.subreddit[0]?.topic}/>
+          <p className="text-xs text-gray-400">
+            <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>
+            • Posted by u/{post.username} <TimeAgo date={post.created_at} />
+          </p>
+        </div>
+
+        {/* Body */}
+        <div className="py-4">
+          <h2 className="text-xl font-semibold">{post.title}</h2>
+          <p className="mt-2 text-sm font-light">{post.body}</p>
+        </div>
+
+        {/* Image */}
+        <img className="w-full" src={post.image} alt="" />
+        {/* Footer */}
+        <div className="flex space-x-4 text-gray-400">
+          <div>
+            <ChatAltIcon className="h-6 w-6"/>
+            <p className="hidden sm:inline">{post.comments.length} Comments</p>
+          </div>
+          <div>
+            <GiftIcon className="h-6 w-6"/>
+            <p className="hidden sm:inline">Award</p>
+          </div>
+          <div>
+            <ShareIcon className="h-6 w-6"/>
+            <p className="hidden sm:inline">Share</p>
+          </div>
+          <div>
+            <BookmarkIcon className="h-6 w-6"/>
+            <p className="hidden sm:inline">Save</p>
+          </div>
+          <div>
+            <DotsHorizontalIcon className="h-6 w-6"/>
+            <p className="hidden sm:inline"></p>  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
