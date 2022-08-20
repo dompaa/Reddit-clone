33db23296bb9b0e56a7573804ad914dc8f659c6f
Dominik <gradiskid@gmail.com>
2022-08-20 22:32:11 +0200

    linking user to post topic

--- components/Post.tsx
+++ components/Post.tsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/outline";
 import Avatar from "./Avatar";
 import TimeAgo from 'react-timeago'
+import Link from 'next/link'
 
 type Props = {
   post: Post;
@@ -17,6 +18,7 @@ type Props = {
 
 function Post({post}: Props) {
   return (
+    <Link href={`/post/${post.id}`}>
     <div className="flex cursor-pointer rounded-md border-gray-300 bg-white
     shadow-sm hover:border-gray-600">
       {/* Votes */}
@@ -33,7 +35,11 @@ function Post({post}: Props) {
         <div className="flex items-center space-x-2">
           <Avatar seed={post.subreddit[0]?.topic}/>
           <p className="text-xs text-gray-400">
-            <span className="font-bold text-black hover:text-blue-400 hover:underline">r/{post.subreddit[0]?.topic}</span>
+            <Link href={`/subbreddit/${post.subreddit[0]?.topic}`}>
+            <span className="font-bold text-black hover:text-blue-400 hover:underline">
+              r/{post.subreddit[0]?.topic}
+            </span>
+            </Link>{' '}
             • Posted by u/{post.username} <TimeAgo date={post.created_at} />
           </p>
         </div>
@@ -71,6 +77,7 @@ function Post({post}: Props) {
         </div>
       </div>
     </div>
+    </Link>
   );
 }
 
