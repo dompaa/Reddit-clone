a9e6f466829ca185af736682194ea4433e2226ee
Dominik <gradiskid@gmail.com>
2022-08-21 23:57:48 +0200

    first commit

---
+++ pages/post/[postid].tsx
@@ -0,0 +1,25 @@
+import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
+import React from 'react'
+import Post from '../../components/Post'
+import { GET_POSTS_BY_POST_ID } from '../../graphql/queries'
+
+function PostPage() {
+    const router = useRouter()
+    //loading, error by choice
+    const { data } = useQuery(GET_POSTS_BY_POST_ID, {
+        variables: {
+            post_id: router.query.postId,
+        },
+    })
+
+    const post: Post = data?.getPostListByPostId;
+
+  return (
+    <div>
+        <Post post={post}/>
+    </div>
+  )
+}
+
+export default PostPage
