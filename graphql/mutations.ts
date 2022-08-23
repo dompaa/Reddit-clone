a06a3b4ec3f3b8ba92e5dca7894c7d040cd01a5d
Dominik <gradiskid@gmail.com>
2022-08-23 17:59:46 +0200

    added ADD COMMENT mutation

--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -1,7 +1,19 @@
 import { gql } from "@apollo/client";
 
+export const ADD_COMMENT = gql`
+    mutation MyMutation($post_id: ID!, $username: String!, $text: String!) {
+      insertComment(post_id: $post_id, text: $text, username: $username) {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+    }
+`
+
 export const ADD_POST = gql`
-    mutation Mymutation(
+    mutation MyMutation(
         $body: String!
         $image: String!
         $subreddit_id: ID!
