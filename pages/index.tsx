529cc96e40b972394eb17c9c25653d1bdd535722
Dominik <gradiskid@gmail.com>
2022-08-31 01:52:43 +0200

    implemented top community section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Feed from '../components/Feed'
 import PostBox from '../components/PostBox'
+import SubredditRow from '../components/SubredditRow'
+import { GET_SUBREDDITS_WITH_LIMIT } from '../graphql/queries'
 
 const Home: NextPage = () => {
+const { data } = useQuery(GET_SUBREDDITS_WITH_LIMIT, {
+  variables: {
+    limit: 10
+  }
+}) 
+
+  const subreddits: Subreddit[] = data?.getSubredditListLimit
+
   return (
     <div className="max-w-5xl my-7 mx-auto">
       <Head>
@@ -13,9 +24,23 @@ const Home: NextPage = () => {
 
       <PostBox/>
       
-      <div className='flex flex-col'>
+      <div className='flex'>
         {/* Feed */}
         <Feed/>
+
+        <div className='sticky top-36 mx-5 mt-5 hidden h-fit min-w-[300px] rounded-md border
+        border-gray-300 bg-white lg:inline'>
+          <p className='text-md mb-1 p-4 pb-3 font-bold'>Top communities</p>
+
+          <div>
+            {subreddits?.map((subreddit, i) =>(
+              <SubredditRow key={subreddit.id}
+               topic={subreddit.topic}
+               index={i}
+               />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
